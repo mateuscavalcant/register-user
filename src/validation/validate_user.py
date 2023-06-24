07d9e0a748b25c7b6061708ccc484d85235cfeb4
Mateus <mateuscavalcant7@gmail.com>
2023-06-24 13:34:33 -0300

    Create Validate

---
+++ src/validation/validate_user.py
@@ -0,0 +1,12 @@
+import re
+
+def validateEmail(email):
+    if not re.match(r"[^@]+@[^@]+\.[^@]+", email):
+        return False
+    return True
+
+
+def validatePassword(password):
+    if len(password) < 8 or len(password) > 20:
+        return False
+    return True
